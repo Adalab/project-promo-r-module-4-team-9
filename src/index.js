d4205739e607a3147be89aaa3cedf985d4d7b605
Monica Hidalgo Martin <monicahidalgomartin@gmail.com>
2022-12-23 13:35:20 +0100

    Add template engines for user card with html and css

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const server = express();
 // Configuramos el servidor
 server.use(cors()); //sirve para poder acceder desde cualquien navegador
 server.use(express.json());
+server.set('view engine', 'ejs');
 
 // Arrancamos el servidor en el puerto 4000
 const serverPort = 4000;
@@ -18,40 +19,60 @@ server.listen(serverPort, () => {
 });
 
 const savedCards = [];
-console.log(savedCards)
 // Escribimos los endpoints que queramos
 //Endpoint crear tarjeta
 server.post('/card', (req, res) => {
   //
-  if(req.body.palette === '' || req.body.name === '' || req.body.job === '' || req.body.phone === '' || req.body.email === '' || req.body.linkedin === '' || req.body.github === '' || req.body.photo === ''){
+  if (
+    req.body.palette === '' ||
+    req.body.name === '' ||
+    req.body.job === '' ||
+    req.body.phone === '' ||
+    req.body.email === '' ||
+    req.body.linkedin === '' ||
+    req.body.github === '' ||
+    req.body.photo === ''
+  ) {
     const responseError = {
-      error: "Faltan datos",
+      error: 'Faltan datos',
       success: false,
-    }
+    };
     res.json(responseError);
-  }else{
-    const uniq = 'id' + (new Date()).getTime();
-    const newCard = {id: uniq, ...req.body};
+  } else {
+    const uniq = 'id' + new Date().getTime();
+    const newCard = { id: uniq, ...req.body };
     savedCards.push(newCard);
     const responseSuccess = {
-      cardURL: `https://localhost:4000/card/${newCard.id}`,
+      cardURL: `http://localhost:4000/card/${newCard.id}`,
       success: true,
     };
     res.json(responseSuccess);
   }
 });
 
-//Petición para pintar la tarjeta 
-server.get('/card/:id', (req, res) =>{
+//Petición para pintar la tarjeta
+server.get('/card/:id', (req, res) => {
   //Nos devuelve el id del objeto con la URL
-  console.log(req.params.id)
+  console.log(req.params.id);
+  const userCardObject = {
+    palette: '3',
+    name: 'Pepa',
+    job: 'Cake Developer',
+    phone: '123456789',
+    email: 'pepa@cakes.com',
+    linkedin: 'https://www.linkedin.com/in/pepa-sastre-0675222/',
+    github: 'https://www.linkedin.com/in/pepa-sastre-0675222/',
+    photo: 'https://es.web.img2.acsta.net/pictures/14/02/27/12/49/374639.jpg',
+  };
+
   //Hasta que no guardemos saveCards en una base de datos, cada vez que refrescamos o hacemos un cambio en el archivo se vacía
   savedCards.find((card) => card.id === req.params.id);
   //Motor plantillas
-  const htmlCode = "<div>Hola</div>"
-  res.send(htmlCode);
-})
+  res.render('user-card', userCardObject);
+});
 
 //Crear servidor de estáticos
 const staticServer = './src/public-react';
 server.use(express.static(staticServer));
+const serverStyles = './src/public-css';
+server.use(express.static(serverStyles));
